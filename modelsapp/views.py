a1eaf9c82c1f3bd9b72e88b7327f4f910f477121
Charan Teja Nemalladinne <cn21043@corp.ojas-it.com>
2021-04-27 16:33:15 +0530

    views was updated

--- modelsapp/views.py
+++ modelsapp/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+from .models import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .Serialization import Student_serializer
 
-def fun(request):
-    s="this is module"
-    return httpresponse(s)
+@api_view(['GET', 'POST'])
+def create_view(request):
+    if request.method == 'GET':
+        s = Student.objects.all()
+        print(s)
+
+        Es = Student_serializer(s, many=True)
+        print(Es.data)
+        return Response(Es.data)
+    elif request.method == 'POST':
+        d = request.data
+        s = Student_serializer(data=d)
+        if s.is_valid():
+            s.save()
+            return Response('success')
+
+#
+def js_view(request):
+    x = {
+      "name": "John",
+      "age": 30,
+      "city": "New York",
+   }
+
+    print(x)
+    y = json.dumps(x)
+    return HttpResponse(y)
 
 
-# Create your views here.
+
